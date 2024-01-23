7edd8beb83fa5163eefc916435830e244bb66c24
Dima Boyko <dimatmg@gmail.com>
2024-01-23 16:42:25 +0200

    v5

--- slider_dix/slider_dix.js
+++ slider_dix/slider_dix.js
@@ -1,8 +1,9 @@
+//******************************************************
 /*
-* Dmitry Boyko
-*04.12.2013
-*24.12.2013
-*tmgsoft@hotmail.com
+ * Dmitry Boyko
+ * 04.12.2013
+ * 24.12.2013
+ * tmgsoft@hotmail.com
 */
 //******************************************************
 /*
@@ -18,6 +19,7 @@ sLook - пауза после ручного выбора (пропуск шагов)
 
 function SliderDIX(sName,sCount,sMargin,sInterval,sAuto_interval,sLook)
 {
+
 //******************************************************
 //                      НАСТРОЙКИ
 var count=sCount;           //картинок видимых в слайдере
@@ -28,50 +30,62 @@ var Look=sLook;//пауза при ручном выборе
 //******************************************************
 var width_ls='';          //ширина слайдера
 var step =margin;        //перемещение картинки
-var loced=true;          //блокировать при анимации
+var loced=false;          //блокировать при анимации
 var element_count=0;     //всех елементов в листалке
 var element_left=0;
 var animate_cont="next";//направление анимации
-var pause_step=0;       //пропуск шагов после ручного выбора блока
-var pause=false;        //остановить анимацию
+var user_control=false;//ручная прокрутка
+//var pause_step=0;       //пропуск шагов после ручного выбора блока
+//var pause=false;        //остановить анимацию
 var slider_loked=false;//блокировка анимации когда не активна страница
+//var auto_slider=0;
+var time=new Date().getTime(); //текущее время в UNIX
+var time_pause=0; //остановить прокрутку слайдера в UNIX
 //******************************************************
 //Выполнить при запуске
-sName='#'+sName+' ';//отступ в CSS + ID
+sName='#'+sName+' ';//ID  + отступ в CSS
   //позиция каждого блока при загрузке
-  $(sName+'img').each(function(i){
-    $(sName+'img:eq('+i+')').css('left',i*margin) ;
+  jQuery(sName+'img').each(function(i){
+    jQuery(sName+'img:eq('+i+')').css('left',i*margin) ;
   });
-  element_count=$(sName+'img').length;//количество елементов  в листалке
+  element_count=jQuery(sName+'img').length;//количество елементов  в листалке
   //ширина листалки
   width_ls=count* margin;
-  $(sName+'.work').css('width',width_ls+'px') ;
-
-//****************************************************** 
-  // Автоматический скролинг
-  // Шаг - 1 анимация слайдера
-  var auto_slider=setInterval(function(){
-    if(pause_step==0 && slider_loked==false)//анимация
-    {
-        controler(animate_cont);
-        pause_step=auto_interval; //задать паузу при авто прокрутке
-    }
-    else //пропустить анимацию
-    {
-      pause_step--;//щечик шагов
-      pause=false;//сразу отменить паузу
-      if(pause_step<0)pause_step=0;
-    }
-    if(pause==true)pause_step=Look;  //пауза - когда ручной выбор
-    if(animate_cont=="prev")animate_cont="next";// после ручного перехода снова перейти на автоматическу.
-  },interval+100);// + задержка для обработки всех скриптов
+  jQuery(sName+'.work').css('width',width_ls+'px') ;
 
 //******************************************************
+//Прокрутка элементов в слайдере
+time_pause=time+auto_interval; //ожидание прокрутки
+var auto_slider=setInterval(function()
+{
+  time=new Date().getTime(); //отщет времени
+  if(loced==false)  //анимация завершена
+  {
+    //анимация элементов после таймаута
+    //если пользователь не выбрал направление сам
+    //также весь слайдер блокируется если страница потерялп фокус
+    if(time>time_pause && slider_loked==false || user_control==true)
+    {
+      controler(animate_cont);//анимация
+      if(user_control==true)   //настроки после ручного выбора элемента
+      {
+        time_pause=time+Look;  //задержка чтобы посмотреть на элементы
+        user_control=false;    //слейдер переключается после задержки в автоматический режим
+        animate_cont="next";   //вернутся к стандартному направлению элементов
+      }
+      else                     //настройки автоматического режима
+      {
+        time_pause=time+auto_interval; //интервал для автоматической анимации
+      }
+    }
+  }
+},300);//интервал для проверки
+//******************************************************
   //если страница с слайдером не активна то слайдер остановлен
-  $(window).bind('blur', function() {
+  jQuery(window).bind('blur', function() {
     slider_loked=true;
   });
-  $(window).bind('focus', function() {
+  jQuery(window).bind('focus', function() {
     slider_loked=false;
   });
 
@@ -79,16 +93,19 @@ sName='#'+sName+' ';//отступ в CSS + ID
 
   //******************************************************
   // ручной переход между картинками
-  $(function(){
-    $(sName+'#prev').click(function(){
+  jQuery(function(){
+    jQuery(sName+'#prev').click(function(){      //назад
       animate_cont='prev';
-      pause_step=0;
-      pause=true;
+      user_control=true;
     });
-    $(sName+'#next').click(function(){
+
+    jQuery(sName+'#next').click(function(){      //вперед
       animate_cont='next';
-      pause_step=0;
-      pause=true;
+      user_control=true;
+    });
+
+    jQuery(sName+'img').click(function(){        //остановить для посмотра
+      time_pause=time+Look;
     });
   });
 
@@ -98,8 +115,8 @@ sName='#'+sName+' ';//отступ в CSS + ID
   {
     var left=0;
     //поочередное пролистывание блоков
-    $(sName+'img').each(function(i){
-        left=$(sName+'img:eq('+i+')').position().left;
+    jQuery(sName+'img').each(function(i){
+        left=jQuery(sName+'img:eq('+i+')').position().left;
         if(direction=='prev')//в лево
         {
           left=left-step;
@@ -109,11 +126,11 @@ sName='#'+sName+' ';//отступ в CSS + ID
           left=left+step;
         }
         // Анимация
-        $(sName+'img:eq('+i+')').animate({
+        jQuery(sName+'img:eq('+i+')').animate({
           left: left+'px',
         },interval);
       });
-      setTimeout(function(){loced=true},interval) ;  //следующий
+      setTimeout(function(){loced=false},interval) ;  //следующий
   }
 
 
@@ -122,14 +139,14 @@ sName='#'+sName+' ';//отступ в CSS + ID
   //порядок размещения картинок в слайдере перед анимацией
   function controler(direction)
   {
-    loced=false;
+    loced=true;
     var left=0; //позиция елемента в слайдере
     element_left=-1;
     //Показывать только елементы участвующие в анимации
-    $(sName+'img').each(function(i){
-      left=$(sName+'img:eq('+i+')').position().left;
-      if(left<0 || left>width_ls-margin)$(sName+'img:eq('+i+')').css('display','none'); //все елементы вне слайдера скрыть
-      if(left==0 && element_left==-1 && $(sName+'img:eq('+i+')').css('display')=='block') element_left=i;  //первый элемент в слайдере
+    jQuery(sName+'img').each(function(i){
+      left=jQuery(sName+'img:eq('+i+')').position().left;
+      if(left<0 || left>width_ls-margin)jQuery(sName+'img:eq('+i+')').css('display','none'); //все елементы вне слайдера скрыть
+      if(left==0 && element_left==-1 && jQuery(sName+'img:eq('+i+')').css('display')=='block') element_left=i;  //первый элемент в слайдере
     });
     //*****************************************
     //Следующий выежающий елемент, сделать видимым
@@ -142,16 +159,16 @@ sName='#'+sName+' ';//отступ в CSS + ID
       //Прощитываем так, берем от видимого левого элемента
       //отнимаем количество невидимых
       if(element_next>element_count-1) element_next= element_left-(element_count-count);
-      $(sName+'img:eq('+element_next+')').css('display','block');
-      $(sName+'img:eq('+element_next+')').css('left',width_ls) ;//разместить последним
+      jQuery(sName+'img:eq('+element_next+')').css('display','block');
+      jQuery(sName+'img:eq('+element_next+')').css('left',width_ls) ;//разместить последним
     }
     else //следующий элемент размещается с лева
     {
       element_next=element_left-1;  // прокрутка элемента с лева
       //если элементы с лева закончились
       if(element_next<0) element_next= element_count-1; // берем самый правый элемент (последний)
-      $(sName+'img:eq('+element_next+')').css('display','block');
-      $(sName+'img:eq('+element_next+')').css('left',-margin) ; //разместить перед всеми элементами
+      jQuery(sName+'img:eq('+element_next+')').css('display','block');
+      jQuery(sName+'img:eq('+element_next+')').css('left',-margin) ; //разместить перед всеми элементами
     }
     animate(direction); // Анимация
 
